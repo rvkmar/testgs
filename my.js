f92c18106a7cb1bac2300c5676b9d7e6b34d3f2f
rvkmar <rvkmar@gmail.com>
2023-01-22 20:53:35 +0530

    Update my.js

--- my.js
+++ my.js
@@ -20,7 +20,7 @@ function addGS(){
         },
         redirect: 'follow', // manual, *follow, error
         // referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-        body: JSON.stringify({name:"Ravikumar Rajabhather"}) // body data type must match "Content-Type" header
+        body: JSON.stringify({first:"Lochan", last:"Charan", phone:"9444036837"}) // body data type must match "Content-Type" header
     });
 }
 
